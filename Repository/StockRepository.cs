1a01a4fc7d425633088509f178b77b85b4e728fe
BaranKurdoglu <barankurdoglu01@gmail.com>
2026-03-05 23:52:54 +0300

    Add sorting support to stock queries via QueryObject
    Added SortBy and isDescending to QueryObject for flexible sorting. Updated StockRepository to sort stocks by Symbol in ascending or descending order based on query parameters. Included clarifying comments on string comparison and filtering logic.

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -54,7 +54,16 @@ namespace dotnetDeneme.Repository
 
             if (!string.IsNullOrWhiteSpace(query.CompanyName))
             {
-                stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
+                stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName)); //Contains, == ile aynı anlama geliyor.
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase)) //Büyük - Küçük harf farkını yok sayar. 
+                {
+                    stocks = query.isDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
             }
 
 
